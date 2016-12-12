7148665f988667e4f638ad91464f5072ffb84a17
Romain Lenzotti <rom.lenzotti@gmail.com>
2016-12-12 18:14:15 +0100

    Remove promise on fileUtils

---
+++ app.ts
@@ -0,0 +1,4 @@
+
+import {FileUtils} from './src/utils/FileUtils';
+
+new FileUtils("bordeaux")
---
+++ src/utils/FileUtils.ts
@@ -0,0 +1,121 @@
+let path = require('path'),
+    appDir = path.resolve('./');
+
+import * as Fs from "fs";
+import {EventEmitter} from "events";
+
+export interface IFileCallbackSuccess {
+    (content: string): void
+}
+
+export class FileUtils extends EventEmitter {
+
+    private fileName: string;
+    private _stats;
+    private _fd;
+
+    constructor(fileName: string) {
+        super();
+        this.fileName = path.resolve(`${appDir}/resources/${fileName}`);
+    }
+
+    public getFileName = () => this.fileName;
+    /**
+     *
+     * @returns {Promise<string>}
+     */
+    read(resolve: IFileCallbackSuccess = () => {}) {
+
+        const onError = (err) => this.emit('error', err);
+
+        this.exists((stats: Fs.Stats) => {
+
+            this.open("r", (fd) => {
+                this.readFile(resolve, onError);
+            }, onError);
+        }, onError);
+
+    }
+
+    /**
+     *
+     */
+    private exists(success, reject = new Function): void {
+
+        Fs.exists(this.fileName, (exists) => {
+
+            if (exists) {
+                success();
+            } else {
+                reject(new Error('File not found'));
+            }
+        });
+
+    }
+
+    /**
+     *
+     * @returns {Promise<Fs.Stats>}
+     */
+    private stats(resolve: (stats: Fs.Stats) => void, reject = new Function): void {
+
+        Fs.stat(this.fileName, (err, stats) => {
+
+            if(err) {
+                reject(err);
+                return;
+            }
+
+            this._stats = stats;
+            resolve(stats);
+        });
+    }
+
+    /**
+     *
+     * @param flags
+     * @param resolve
+     * @param reject
+     * @returns {Promise<number>}
+     */
+    private open(flags: string = "r", resolve = new Function, reject = new Function): void {
+
+        Fs.open(this.fileName, flags, (err, fd) => {
+
+            if(err) {
+                reject(err);
+                return;
+            }
+            this._fd = fd;
+            resolve(fd);
+        });
+    }
+
+    /**
+     *
+     * @returns {Promise<string>}
+     * @param resolve
+     * @param reject
+     */
+    private readFile(resolve: (content: string) => void, reject = new Function): void {
+
+        let buffer = new Buffer(this._stats.size);
+
+        Fs.read(this._fd, buffer, 0, buffer.length, null, (err, bytesRead, buffer) => {
+
+            if(err) {
+                reject(err);
+                return;
+            }
+
+            const data = buffer.toString("utf8", 0, buffer.length);
+
+            Fs.close(this._fd);
+
+            resolve(data);
+
+        });
+
+
+    }
+}
