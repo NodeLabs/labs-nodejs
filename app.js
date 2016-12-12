4084f23d9279df7322be2fbbc288ee78bbcb8a55
Romain Lenzotti <rom.lenzotti@gmail.com>
2016-12-12 16:35:48 +0100

    Commit Final branch

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 "use strict";
 require('source-map-support').install();
-var server_1 = require("./server"); // export default
-new server_1.default(8080).start();
+new Servr(8080).start();
 //# sourceMappingURL=app.js.map
